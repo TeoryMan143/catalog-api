1a39499a4ca457ab0117f6180c0e132f7a8b860a
TeoryMan <jonathancortestm143@gmail.com>
2023-11-12 17:04:28 -0500

    Fix Postgres config

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,13 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'https://deno.land/x/postgresjs@v3.4.3/mod.js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 import { load } from 'deno/dotenv/mod.ts';
 
 const env = await load();
-
-const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
-migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' });
 
 const connection = postgres(env.DATABASE_URL);
 
